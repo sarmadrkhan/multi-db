2dc310abe9d2428b756775c6dcd99ee9eac697cd
Sarmad Raees Khan <sarmad.raees.ext@openeconomics.eu>
2025-09-01 14:39:27 +0200

    add new modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
