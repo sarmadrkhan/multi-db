c650d5db961acc38152e087b600d38bc4e4e35fa
Sarmad Raees Khan <sarmad.raees.ext@openeconomics.eu>
2025-09-01 14:40:30 +0200

    define user crud

---
+++ src/user/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PostgresPrismaService } from 'src/prisma/postgres.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PostgresPrismaService) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
