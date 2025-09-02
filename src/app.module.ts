c0c9e1ebc5903e9020717cf8e733b160335844fa
Sarmad Raees Khan <sarmad.raees.ext@openeconomics.eu>
2025-09-02 10:25:43 +0200

    update app module with product module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UserModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
