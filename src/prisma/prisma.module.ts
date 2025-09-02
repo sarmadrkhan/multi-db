77a7da07a9670bf23bbbf3c28437e26514be0fd0
Sarmad Raees Khan <sarmad.raees.ext@openeconomics.eu>
2025-09-02 17:23:32 +0200

    hook MysqlPrismaModule to prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PostgresPrismaService } from './postgres.service';
 import { MariaPrismaService } from './mariadb.service';
+import { MysqlPrismaService } from './mysql.service';
 
 @Global()
 @Module({
-  providers: [PostgresPrismaService, MariaPrismaService],
-  exports: [PostgresPrismaService, MariaPrismaService],
+  providers: [PostgresPrismaService, MariaPrismaService, MysqlPrismaService],
+  exports: [PostgresPrismaService, MariaPrismaService, MysqlPrismaService],
 })
 export class PrismaModule {}
