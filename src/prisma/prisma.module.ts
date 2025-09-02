a46fd008a611104912f70dc2cd2ed9c4d7c9fa06
Sarmad Raees Khan <sarmad.raees.ext@openeconomics.eu>
2025-09-02 10:26:12 +0200

    hook mariadb service to the prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PostgresPrismaService } from './postgres.service';
+import { MariaPrismaService } from './mariadb.service';
 
 @Global()
 @Module({
-  providers: [PostgresPrismaService],
-  exports: [PostgresPrismaService],
+  providers: [PostgresPrismaService, MariaPrismaService],
+  exports: [PostgresPrismaService, MariaPrismaService],
 })
 export class PrismaModule {}
